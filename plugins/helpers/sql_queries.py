af706dae4b853f1bbedf183560345ec91e661e73
root <raymondkalonji@gmail.com>
2020-09-25 15:41:11 +0000

    feat: added sql statements creating tables

---
+++ plugins/helpers/sql_queries.py
@@ -0,0 +1,28 @@
+
+create_accounts = f"""
+    drop table if exists accounts;
+    create table accounts (
+        year int
+    );
+    """
+
+create_delinquencies_fact = f"""
+    drop table if exists delinquencies;
+    create table delinquencies (
+        year int
+    );
+    """
+
+create_finances_fact = f"""
+    drop table if exists finances;
+    create table finances (
+        year int
+    );
+    """
+
+create_demographics_fact = f"""
+    drop table if exists demographics;
+    create table demographics (
+        year int
+    );
+    """
