ea66f9670e1345ac5ca3d90d09c28d6e05c987c8
root <raymondkalonji@gmail.com>
2020-09-27 13:46:31 +0000

    refactor: reorganizing tasks order

--- dags/credilytics.py
+++ dags/credilytics.py
@@ -23,7 +23,6 @@ dag = DAG('credilytics',
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
-create_tables = DummyOperator(task_id='create_tables',  dag=dag)
 transform_data = TransformOperator(
     task_id='transform_data',
     s3_bucket='credilytics',
@@ -33,6 +32,9 @@ transform_data = TransformOperator(
     aws_credentials_id='aws_credentials',
     dag=dag
 )
+
+create_tables = DummyOperator(task_id='create_tables',  dag=dag)
+
 load_accounts_fact = DummyOperator(task_id='load_accounts_fact',  dag=dag)
 load_delinquencies_dimension = DummyOperator(task_id='load_delinquencies_dimension',  dag=dag)
 load_finances_dimension = DummyOperator(task_id='load_finances_dimension',  dag=dag)
@@ -41,9 +43,9 @@ check_data_quality = DummyOperator(task_id='check_data_quality',  dag=dag)
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-start_operator >> create_tables
-create_tables >> transform_data
-transform_data >> load_accounts_fact
+start_operator >> transform_data
+transform_data >> create_tables
+create_tables >> load_accounts_fact
 load_accounts_fact >> load_delinquencies_dimension >> check_data_quality
 load_accounts_fact >> load_finances_dimension >> check_data_quality
 load_accounts_fact >> load_demographics_dimension >> check_data_quality
